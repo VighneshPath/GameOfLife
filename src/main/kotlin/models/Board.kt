297f4f23a81c31049fa651a6220a19e17188dc23
VighneshPath <pathrikarvighnesh@gmail.com>
2023-02-24 15:18:19 +0530

    Fix neighbor logic for board

--- src/main/kotlin/models/Board.kt
+++ src/main/kotlin/models/Board.kt
@@ -21,38 +21,29 @@ class Board(private var boardState : MutableList<MutableList<Cell>>){
     }
 
     private fun getNearestLiveNeighbours(row: Int, column: Int, boardState: List<List<Cell>>): Int {
-        // check right neighbor
-        var count: Int = 0
-        println(row)
-        println(column)
-        println(boardState.size)
-        print(boardState[row][column])
-        if(column < boardState.size-1){
-            if(boardState[row][column+1].isLive()){
-                count+=1
-            }
-        }
-        // check left neighbor
-        if(column>0){
-            if(boardState[row][column-1].isLive()){
-                count+=1
-            }
-        }
+        val offsets = listOf(Pair(-1, -1),
+            Pair(-1, 0),
+            Pair(-1, 1),
+            Pair(0, -1),
+            Pair(0, 1),
+            Pair(1, -1),
+            Pair(1, 0),
+            Pair(1, 1))
 
-        // check top neighbor
-        if(row < boardState.size-1){
-            if(boardState[row+1][column].isLive()){
+        println("$row $column")
+        var count = 0
+
+        for(offset in offsets){
+            val k = row + offset.first
+            val l = column + offset.second
+
+            if(0<=k && k<boardState.size &&
+                0<=l && l<boardState.size &&
+                boardState[k][l].isLive()){
                 count+=1
             }
         }
-
-        // check bottom neighbor
-        if(row > 0){
-            if(boardState[row-1][column].isLive()){
-                count+=1
-            }
-        }
-
+        println(count)
         return count
     }
 
--- src/test/kotlin/models/BoardTest.kt
+++ src/test/kotlin/models/BoardTest.kt
@@ -10,26 +10,43 @@ import kotlin.test.assertIs
 
 class BoardTest{
     private lateinit var board: Board
-    @BeforeEach
-    fun setUp(){
+    @Test
+    fun `should die after two iterations`(){
         val boardSize = 3
-        val liveCellLocations = listOf(Pair(0, 1), Pair(0, 2))
-
+        val liveCellLocations = listOf(Pair(1, 0), Pair(1, 1), Pair(1, 2))
         val boardDetails = MutableList(boardSize){ MutableList<Cell>(boardSize){DeadCell()} }
-
-        println(boardDetails.size)
         for(location in liveCellLocations){
             boardDetails[location.first][location.second] = LiveCell()
         }
-
         board = Board(boardDetails)
+
+        val returnedBoard = board.startGame(1)
+
+
+        assertIs<DeadCell>(returnedBoard[0][0])
+        assertIs<LiveCell>(returnedBoard[0][1])
+        assertIs<DeadCell>(returnedBoard[0][2])
+        assertIs<DeadCell>(returnedBoard[1][0])
+        assertIs<DeadCell>(returnedBoard[1][2])
+        assertIs<LiveCell>(returnedBoard[1][1])
+        assertIs<LiveCell>(returnedBoard[2][1])
     }
 
     @Test
-    fun `should die after one iteration`(){
-        val returnedBoard = board.startGame(1)
+    fun `should have one cell after one iteration`(){
+        val boardSize = 3
+        val liveCellLocations = listOf(Pair(1, 0), Pair(1, 1), Pair(1, 2))
+        val boardDetails = MutableList(boardSize){ MutableList<Cell>(boardSize){DeadCell()} }
+        for(location in liveCellLocations){
+            boardDetails[location.first][location.second] = LiveCell()
+        }
+        board = Board(boardDetails)
 
-        assertIs<DeadCell>(returnedBoard[0][1])
-        assertIs<DeadCell>(returnedBoard[0][2])
+        val returnedBoard = board.startGame(2)
+
+        assertIs<LiveCell>(returnedBoard[1][0])
+        assertIs<LiveCell>(returnedBoard[1][2])
+        assertIs<LiveCell>(returnedBoard[1][1])
     }
+
 }
